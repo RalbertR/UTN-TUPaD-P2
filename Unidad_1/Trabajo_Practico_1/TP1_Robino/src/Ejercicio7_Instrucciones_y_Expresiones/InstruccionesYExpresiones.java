b4bc47357db48f7b4af3a51545c0555d436672df
RalbertR <raulrobino@gmail.com>
2025-08-30 07:57:32 -0300

    Finalizado TP1

---
+++ Unidad_1/Trabajo_Practico_1/TP1_Robino/src/Ejercicio7_Instrucciones_y_Expresiones/InstruccionesYExpresiones.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio7_Instrucciones_y_Expresiones;
+
+/**
+ *
+ * @author ralbe
+ */
+public class InstruccionesYExpresiones {
+    public static void main(String[] args) {
+    int num = 1;
+    num = num + 5;      
+    System.out.println(num);
+    
+    /*
+        Las expresiones son fragmentos que generan valor, mientras que las instrucciones
+        son sentencias completas que indican al programa que hacer con esos valores. -
+    */
+    }
+}
